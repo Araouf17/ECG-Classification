a3d61bdfb5221b4f339cb810ac63d85c83f92286
andrewgamal1234 <83845723+andrewgamal1234@users.noreply.github.com>
2022-06-26 15:27:18 +0200

    Add files via upload

---
+++ mouse controller.py
@@ -0,0 +1,17 @@
+from time import sleep
+import pyautogui
+
+print(pyautogui.size())
+
+# while(True):
+#      print(pyautogui.position())
+#      sleep(2)
+
+pyautogui.moveTo(960, 540, 4)
+pyautogui.moveTo(960, 0, 4)
+pyautogui.moveTo(960, 540, 4)
+pyautogui.moveTo(960, 1080, 4)
+#print(pyautogui.onScreen(x,y))
+
+#pyautogui.moveTo(500,600)
+
